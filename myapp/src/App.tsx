8bf8921f3c027644c5a29fe3e2c99fed84640ae7
junman95 <junman95@naver.com>
2024-06-23 23:36:43 +0900

    feat: exporter example

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,27 +1,56 @@
 import './App.css';
+import * as THREE from 'three';
 
-import { Canvas } from '@react-three/fiber';
-import { Html } from '@react-three/drei';
+import { Canvas, useThree } from '@react-three/fiber';
+import { Box, OrbitControls } from '@react-three/drei';
+import { GLTFExporter } from 'three/addons/exporters/GLTFExporter.js';
+
+const Foo = () => {
+  const exporter = new GLTFExporter();
+  const options = {
+    binary: true
+  };
+  const state = useThree();
+  console.log(state.scene);
+  exporter.parse(
+    state.scene,
+    (result) => {
+      console.log(result instanceof ArrayBuffer);
+      downloadJSON(result, 'scene.gltf');
+    },
+    (e) => {
+      console.log(e);
+    },
+    options
+  );
+  return null;
+};
 
 function App() {
   return (
     <div style={{ height: '100vh', width: '100vw', position: 'relative' }}>
       <Canvas camera={{ position: [0, 0, 22.5] }}>
-        <Html
-          fullscreen
-          style={{ userSelect: 'none' }}
-          castShadow
-          receiveShadow
-        >
-          <iframe
-            width={'100%'}
-            height={'100%'}
-            src="https://www.tmaxmetaai.com/aboutus"
-          ></iframe>
-        </Html>
+        <Foo />
+        <Box
+          args={[1, 1, 1]} // Args for the buffer geometry
+          material={new THREE.MeshBasicMaterial({ color: 'red' })} // Material
+        />
+        <OrbitControls />
       </Canvas>
     </div>
   );
 }
 
 export default App;
+function downloadJSON(
+  result: ArrayBuffer | { [key in string]: any },
+  fileName: string
+) {
+  const blob = new Blob([result], { type: 'application/octet-stream' });
+  console.log(blob);
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = fileName;
+  a.click();
+}
